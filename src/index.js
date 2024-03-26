e708bab4720216b028f980536dc9d723cf260156
Jeany-hlangabeza <jeanyh144@gmail.com>
2024-03-26 23:23:58 +0000

    axios
    added axios url to html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+let apiKey = "d34702155fe628t713faof5f645213cb";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query= ${city}&key= ${apiKey}`;
+
 function searchBox(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
@@ -7,3 +10,10 @@ function searchBox(event) {
 
 let cityForm = document.querySelector("#search-form");
 cityForm.addEventListener("submit", searchBox);
+
+function showTemperature(response) {
+  let city = response.data.ciy;
+  console.log(city);
+}
+
+axios.get(apiUrl).then(showTemperature);
