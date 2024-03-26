f14d9471b5f9848a8e43b04906854d5f2b19e1fe
Jeany-hlangabeza <jeanyh144@gmail.com>
2024-03-26 23:50:13 +0000

    Update index.js
    added a separate function for the API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-let apiKey = "d34702155fe628t713faof5f645213cb";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query= ${city}&key= ${apiKey}`;
+function showResponse(response) {
+  let city = "response.data.city";
+}
+
+function updatedCity(city) {
+  let apiKey = "d34702155fe628t713faof5f645213cb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showResponse);
+}
 
 function searchBox(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let currentCity = document.querySelector("#city");
   currentCity.innerHTML = searchInput.value;
+  updatedCity(searchInput.value);
 }
 
 let cityForm = document.querySelector("#search-form");
 cityForm.addEventListener("submit", searchBox);
-
-function showTemperature(response) {
-  let city = response.data.ciy;
-  console.log(city);
-}
-
-axios.get(apiUrl).then(showTemperature);
