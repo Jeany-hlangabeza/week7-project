4e4fb087d456beda256e8105107caa614affb019
Jeany-hlangabeza <jeanyh144@gmail.com>
2024-03-27 00:14:36 +0000

    updated javascript
    added id to html and changed the temperature to real time temperature using Api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-function showResponse(response) {
-  let city = "response.data.city";
+function showTemperature(response) {
+  let temperature = document.querySelector("#temperature");
+  let currentTemperature = response.data.temperature.current;
+  temperature.innerHTML = Math.round(currentTemperature);
 }
 
 function updatedCity(city) {
   let apiKey = "d34702155fe628t713faof5f645213cb";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showResponse);
+  axios.get(apiUrl).then(showTemperature);
 }
 
 function searchBox(event) {
